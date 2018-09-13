632b73a629fb6547fc413dd2393f89398416aea2
Marc Lehmann <marc.lehmann@rwth-aachen.de>
2018-09-13 18:17:49 +0200

    Give peers IDs

--- prototype/prototype.py
+++ prototype/prototype.py
@@ -1,20 +1,49 @@
 import simpy
+import bitstring as bs
+import random
 
 class Peer:
-    def __init__(self, env):
+    ID_LENGTH = 16
+
+    def __init__(self, env, peer_id):
         self.env = env
+        self.peer_id = peer_id
+        self.peers = {}
 
-        env.process(self.request_generator())
+    def introduce(self, peer):
+        if peer.peer_id == self.peer_id:
+            return
+        self.peers[peer.peer_id] = peer
 
-    def request_generator(self):
+    def handle_request(self, query_peer_id):
+        print('{} at time {}: handling request for {}'.format(self.peer_id,
+                                                              env.now,
+                                                              query_peer_id))
+
+def request_generator(peers, peer):
+    while True:
+        if len(peers) <= 1:
+            break
         while True:
-            yield env.timeout(1)
-            self.handle_request()
-
-    def handle_request(self):
-        print('handling request at {}'.format(env.now))
+            query_peer_id = random.sample(peers.keys(), 1)[0]
+            if query_peer_id != peer.peer_id:
+                break
+        peer.handle_request(query_peer_id)
+        yield env.timeout(1)
 
 if __name__ == '__main__':
     env = simpy.Environment()
-    Peer(env)
+    peers = {}
+    for i in range(10):
+        while True:
+            peer_id_uint = random.randrange(2 ** Peer.ID_LENGTH)
+            peer_id = bs.Bits(uint=peer_id_uint, length = Peer.ID_LENGTH)
+            if peer_id not in peers:
+                peer = Peer(env, peer_id)
+                peers[peer_id] = peer
+                env.process(request_generator(peers, peer))
+                break
+    for peer in peers.values():
+        for other_peer in peers.values():
+            peer.introduce(other_peer)
     env.run(until=float('inf'))
