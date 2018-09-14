926e81914068f820234fdde1f9bc7eaf46d7a6f9
Marc Lehmann <marc.lehmann@rwth-aachen.de>
2018-09-14 19:44:11 +0200

    Use prefix
    Peers only store an unlimited amount of other peers with whom they share
    a prefix (current hardcoded prefix length of 2). These are the sync
    peers, the ones that will later on exchange updates about records
    they're responsible for. For now, these records are references to each
    other (and there are no updates).
    
    In addition, every peer stores, for every subprefix of its prefix that
    diverges in the least significant bit, a limited number of query peers.
    These are the ones to which queries will be issued about records (i.e.
    currently references) concerning peers that are not sync peers.
    
    In order to obtain that information, query peers must answer queries.
    This is not yet implemented.

--- prototype/prototype.py
+++ prototype/prototype.py
@@ -4,21 +4,57 @@ import random
 
 class Peer:
     ID_LENGTH = 16
+    MAX_QUERY_PEERS = 8
 
     def __init__(self, env, peer_id):
         self.env = env
         self.peer_id = peer_id
-        self.peers = {}
+        self.prefix = self.peer_id[:2]
+        self.query_peers = {}
+        self.sync_peers = {}
 
     def introduce(self, peer):
         if peer.peer_id == self.peer_id:
             return
-        self.peers[peer.peer_id] = peer
+        if peer.peer_id.startswith(self.prefix):
+            self.sync_peers[peer.peer_id] = peer
+            return
+        for i in range(min(len(self.prefix), len(peer.prefix))):
+            if self.prefix[i] != peer.prefix[i]:
+                subprefix = peer.prefix[:i+1]
+                if peer.prefix in self.query_peers:
+                    existing_peers = self.query_peers[subprefix]
+                    if len(existing_peers) < Peer.MAX_QUERY_PEERS:
+                        existing_peers.append(peer)
+                else:
+                    self.query_peers[subprefix] = [peer]
+                break
 
-    def handle_request(self, query_peer_id):
-        print('{} at time {}: handling request for {}'.format(self.peer_id,
-                                                              env.now,
-                                                              query_peer_id))
+    def handle_request(self, query_id):
+        print('{}:{}: request for {} - '.format(self.env.now, self.peer_id,
+                                             query_id), end='')
+        if query_id in self.sync_peers:
+            peer = self.sync_peers[query_id]
+            print('found in sync peers')
+            return
+        for (prefix, query_peers) in self.query_peers.items():
+            # TODO Find the longest matching prefix, not just any.
+            if query_id.startswith(prefix):
+                for query_peer in query_peers:
+                    print('querying {} ... '.format(query_peer.peer_id), end='')
+                    peer = query_peer.query(query_id)
+                    if peer is None:
+                        print('failed')
+                    else:
+                        print('success')
+                        break
+                break
+        else:
+            print('no known peer with prefix matching {}'.format(query_id))
+
+    def query(self, query_id):
+        # TODO
+        return None
 
 def request_generator(peers, peer):
     while True:
