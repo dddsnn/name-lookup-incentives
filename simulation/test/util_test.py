a2e6377d7c3a0b93a8a43f100e8cf0bef480bfc8
Marc Lehmann <marc.lehmann@rwth-aachen.de>
2019-01-24 15:50:38 +0100

    Test network

--- simulation/test/util_test.py
+++ simulation/test/util_test.py
@@ -1,6 +1,7 @@
-import unittest
+import unittest.mock
 import simulation as s
 import bitstring as bs
+import simpy
 
 
 class TestBitOverlap(unittest.TestCase):
@@ -30,6 +31,57 @@ class TestBitOverlap(unittest.TestCase):
                                           bs.Bits('0b0101')))
         self.assertEqual(0, s.bit_overlap(bs.Bits('0b10'),
                                           bs.Bits('0b0101')))
+
+
+class TestNetwork(unittest.TestCase):
+    def setUp(self):
+        self.env = simpy.Environment()
+        self.network = s.Network(self.env)
+        self.peer_a_id = bs.Bits(uint=0, length=16)
+        self.peer_b_id = bs.Bits(uint=1, length=16)
+        self.peer_a = unittest.mock.Mock(spec=s.Peer)
+        self.peer_b = unittest.mock.Mock(spec=s.Peer)
+        self.peer_a_address = self.network.register(self.peer_a)
+        self.peer_b_address = self.network.register(self.peer_b)
+
+    def test_assigns_unique_addresses(self):
+        self.assertNotEqual(self.peer_a_address, self.peer_b_address)
+
+    def test_sends_queries(self):
+        queried_id = bs.Bits(uint=10, length=16)
+        self.network.send_query(self.peer_a_id, self.peer_a_address,
+                                self.peer_b_address, queried_id, 0)
+        self.env.run()
+        self.peer_b.recv_query.assert_called_with(self.peer_a_id, queried_id,
+                                                  0)
+
+    def test_sends_responses(self):
+        self.network.send_response(self.peer_a_id, self.peer_a_address,
+                                   self.peer_b_address, set(), None, 0)
+        self.env.run()
+        self.peer_b.recv_response.assert_called_with(self.peer_a_id, set(),
+                                                     None, 0)
+
+    def test_adds_transmission_delay(self):
+        queried_id = bs.Bits(uint=10, length=16)
+        self.network.send_query(self.peer_a_id, self.peer_a_address,
+                                self.peer_b_address, queried_id, 0)
+        self.assertEqual(self.env.peek(), s.Network.TRANSMISSION_DELAY)
+
+    def test_doesnt_add_transmission_delay_for_messages_to_oneself(self):
+        queried_id = bs.Bits(uint=10, length=16)
+        self.network.send_query(self.peer_a_id, self.peer_a_address,
+                                self.peer_a_address, queried_id, 0)
+        self.assertEqual(self.env.peek(), 0)
+
+    def test_raises_for_unassigned_addresses(self):
+        queried_id = bs.Bits(uint=10, length=16)
+        unassigned_address = self.peer_a_address + self.peer_b_address + 1
+        self.assertNotEqual(unassigned_address, self.peer_a_address)
+        self.assertNotEqual(unassigned_address, self.peer_b_address)
+        with self.assertRaises(s.UnassignedAddressError):
+            self.network.send_query(self.peer_a_id, self.peer_a_address,
+                                    unassigned_address, queried_id, 0)
 
 
 if __name__ == "__main__":
